6acc961b146093d5e8d6b98d5a1523f149da6b21
UTB\andera <apocalysm1@gmail.com>
2017-02-13 11:45:43 +0100

    Added a script map, created two scripts PlayerSingleTon and Player Combat Logic

---
+++ Assets/Scripts/Player SingleTon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSingleTon {
+
+    public static PlayerSingleTon instance
+    {
+        get
+        {
+            if (m_instance == null)
+                m_instance = new PlayerSingleTon();
+
+            return m_instance;
+        }
+    }
+
+    private static PlayerSingleTon m_instance;
+
+    public int strength;
+    public int intelligencest;
+    public int dexterity;
+    public int luck;
+    public int health;
+    public int mana;
+    public int exp;
+}
