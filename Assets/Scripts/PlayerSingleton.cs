145f1f7fe713e1ee87d3bbb74527ab77967409da
UTB\andera <apocalysm1@gmail.com>
2017-02-13 15:32:05 +0100

    Fixed the character from leagcy to humanoid, working with the playercombatlogic and playersingleton

---
+++ Assets/Scripts/PlayerSingleton.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSingleton : MonoBehaviour
+{
+
+    public static PlayerSingleton instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                GameObject prefab = (GameObject)Resources.Load("PlayerSingleton");
+                GameObject created = Instantiate(prefab);
+                DontDestroyOnLoad(created);
+                m_instance = created.GetComponent<PlayerSingleton>();
+            }
+
+            return m_instance;
+        }
+    }
+
+    private static PlayerSingleton m_instance;
+
+    public float playerDmg = 1;
+    public int playerMagicDmg = 1;
+    public int playerHealth = 10;
+    public int playerExp = 0;
+    public int playerInt = 5;
+    public int playerStr = 5;
+    public int playerDex = 5;
+    public int playerLuck = 5;
+    public int playerMana = 10;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Initialized UnitySingleton instance!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
