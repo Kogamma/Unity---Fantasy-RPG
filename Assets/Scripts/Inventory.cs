8f1a7368fbe048bb1c2214d86ba968a291a1513e
Oskar Svensson <oskar_erik@hotmail.com>
2017-02-21 11:30:46 +0100

    Added more inventory funtionality

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -5,24 +5,63 @@ using UnityEngine;
 public class Inventory : MonoBehaviour
 {
     // This is the actual inventory which consists of different InventoryItem
+    [System.NonSerialized]
     public List<InventoryItem> inventory;
 
 	void Start ()
     {
-
-	}
-	
-	void Update ()
-    {
-		
+        inventory = PlayerSingleton.instance.playerInventory;
 	}
 
-    public void RemoveItem(InventoryItem itemToRemove)
+    public void AddItem(InventoryItem itemToAdd)
     {
-        // Removes the specified item from the list
-        inventory.Remove(itemToRemove);
+        // Loops through the inventory
+        for (int i = 0; i < inventory.Count; i++)
+        {
+            // Checks if there is already a item like the one we're adding
+            if (inventory[i].itemName == itemToAdd.itemName)
+            {
+                // If there is less than 99 of those items...
+                if (inventory[i].amountOfItem < 99)
+                {
+                    // ... we can just add to that amount
+                    inventory[i].AddItem();
+                }
+                break;
+            }
+            else
+            {
+                // If we're on the last iteration
+                if(i == inventory.Count - 1)
+                {
+                    // Checks if there are any slots that are vacant in the inventory
+                    if(inventory.Count < PlayerSingleton.instance.inventorySize)
+                    {
+                        inventory.Add(itemToAdd);
+                    }
+                }
+            }
+        }
+    }
+
+    public void RemoveItem(int index)
+    {
+        // Creates a variable for the item that will be destroyed
+        InventoryItem itemToDestroy = inventory[index];
+
+        // If we have more than one of the item we just subtract the amount of it
+        if (inventory[index].amountOfItem > 1)
+        { 
+            inventory[index].amountOfItem--;
+        }
+        // If there is only 1 item left we remove it completely from the list
+        else
+        {
+            // Removes the specified item from the list
+            inventory.RemoveAt(index);
+        }
 
         // Destroys the item also
-        Destroy(itemToRemove);
+        Destroy(itemToDestroy);
     }
 }
--- Assets/Scripts/InventoryItem.cs
+++ Assets/Scripts/InventoryItem.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryItem : MonoBehaviour
 {
@@ -10,16 +11,39 @@ public class InventoryItem : MonoBehaviour
     // The type of item if it is a 'consumable' or 'equippable' item
     public string itemType = "none";
 
+    // The image that will represent the item in menu's and such
+    public Sprite itemImage;
+
     // How much the item is worth in a shop
     public int value = 0;
 
+    // How many there are of this item currently
+    public int amountOfItem = 0;
+
     // Constructor
-    public InventoryItem(string name, string type, int val)
+    public InventoryItem(string name, string type, Sprite img, int val)
     {
         itemName = name;
 
         type = itemType;
 
+        itemImage = img;
+
         value = val;
+
+        amountOfItem = 1;
+    }
+
+    // Adds another item to the count of items
+    public void AddItem()
+    {
+        // Only the consumable items are stackable so only they can get more than 1
+        if(itemType == "consumable")
+            amountOfItem++;
+        else if(itemType == "equippable")
+        {
+            Debug.Log("Can't add another equippable of the same type in the same slot!");
+            amountOfItem = 1;
+        }
     }
 }
