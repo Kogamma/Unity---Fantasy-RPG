2474932c4e2524ba6b1a310e603b616514af43f9
Tim Rundström <tim_rundstrom_97@hotmail.com>
2017-02-24 12:44:54 +0100

    added a trigger box so you can go to the town

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChangeScene : MonoBehaviour {
+
+    [SerializeField] string sceneName;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider otherObj)
+    {
+        if (otherObj.CompareTag("Player"))
+            SceneManager.LoadScene(sceneName);
+    }
+}
