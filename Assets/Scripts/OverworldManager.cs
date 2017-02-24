4a809b40a5ca3f9273efb112e72618db701289fe
Dennis Karlsson <dennis.k9705@gmail.com>
2017-02-24 15:14:22 +0100

    Unfinished bugfix, will push again soon.

--- Assets/Scripts/OverworldManager.cs
+++ Assets/Scripts/OverworldManager.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 
 public class OverworldManager : MonoBehaviour
 {
+    public List<GameObject> sortedEnemyList;
+
 	void Start ()
     {
         OverworldEnemySingleton.instance.enemies = GameObject.FindGameObjectsWithTag("Enemy").ToList<GameObject>();
-        OverworldEnemySingleton.instance.enemies.OrderBy(enemy => enemy.name);
+        sortedEnemyList = OverworldEnemySingleton.instance.enemies.OrderBy(enemy => enemy.name).ToList();
 
         if (!OverworldEnemySingleton.instance.backFromCombat)
-            OverworldEnemySingleton.instance.shouldDestroy = new List<bool>(new bool[OverworldEnemySingleton.instance.enemies.Count]);
+            OverworldEnemySingleton.instance.shouldDestroy = new List<bool>(new bool[sortedEnemyList.Count]);
 
-        for (int i = 0; i < OverworldEnemySingleton.instance.enemies.Count; i++)
+        for (int i = 0; i < sortedEnemyList.Count; i++)
         {
             if (OverworldEnemySingleton.instance.shouldDestroy[i])
             {
-                OverworldEnemySingleton.instance.enemies[i].SetActive(false);
+                sortedEnemyList[i].SetActive(false);
             }
         }
     }
