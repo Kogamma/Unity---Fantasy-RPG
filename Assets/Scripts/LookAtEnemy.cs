1bc358da9ffd13d962421588701edbf1e8f69a98
Dennis Karlsson <dennis.k9705@gmail.com>
2017-03-01 10:00:19 +0100

    Put back the "Free Character" folder with luberjack NPC

---
+++ Assets/Scripts/LookAtEnemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtEnemy : MonoBehaviour
+{
+    public GameObject target;   // Target to look at
+    public Vector3 offset;      // Distance from target
+
+
+    // LateUpdate is called after Update each frame
+    void Update()
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        transform.position = player.transform.position + offset;
+
+        transform.LookAt(player.transform.position);
+    }
+}
