2162bbdfcbe854235e454cb79e3f2cf0340954fb
Emanuel Holm <emanuel.holm@gmail.com>
2017-03-02 09:54:49 +0100

    Made a start menu scene with start, option and exit button - Option have a empty scene

---
+++ Assets/Scenes/Emanuel Test Scene/ButtonManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour 
+{
+    public void NewPlayBtn(string newPlayLevel)
+    {
+        SceneManager.LoadScene(newPlayLevel);   // starts the game
+    }
+
+    public void OptionBtn(string OptionLevel)
+    {
+        SceneManager.LoadScene(OptionLevel);    // Loads the Option Scene
+    }
+
+    public void ExitBtn()
+    {
+        Application.Quit(); //  Quits the game
+    }
+
+}
