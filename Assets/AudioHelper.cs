b7292dd2c930992e3f1cd8d19c143b8436cec773
UTB\Ohlsab <eyber4@gmail.com>
2017-03-09 10:56:53 +0100

    Added a audio helper and a music helper and made scripts for sfx and music

---
+++ Assets/AudioHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioHelper : MonoBehaviour
+{
+    private static AudioSource m_Source;
+    void Awake()
+    {
+        m_Source = GetComponent<AudioSource>();
+    }
+
+    public static void PlaySound(AudioClip clip, float volMod = 1)
+    {
+        m_Source.pitch = 1;
+        m_Source.PlayOneShot(clip, PlayerSingleton.instance.sfxVol * volMod);
+    }
+    public static void PlaySoundRandomPitch(AudioClip clip, float minPitch, float maxPitch, float volMod = 1)
+    {
+        m_Source.pitch = Random.Range(minPitch, maxPitch);
+        m_Source.PlayOneShot(clip, PlayerSingleton.instance.sfxVol * volMod);
+    }
+}
---
+++ Assets/MusicHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicHelper : MonoBehaviour
+{
+    private static AudioSource m_Source;
+    void Awake()
+    {
+        m_Source.GetComponent<AudioSource>();
+        m_Source.volume = PlayerSingleton.instance.musicVol;
+    }
+    public static void PlaySound(AudioClip clip, float volMod = 1)
+    {
+        m_Source.clip = clip;
+        m_Source.volume = PlayerSingleton.instance.musicVol * volMod;
+        m_Source.Play();
+    }
+}
