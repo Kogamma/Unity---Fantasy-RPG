ccbb9baaa1634bac44ce3ebb0a224d5a4dcfa2fe
Oskar Svensson <oskar_erik@hotmail.com>
2017-03-10 10:24:41 +0100

    Finished navigation with keyboard in the Main Menu

--- Assets/Scenes/Emanuel Test Scene/ButtonManager.cs
+++ Assets/Scenes/Emanuel Test Scene/ButtonManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class ButtonManager : MonoBehaviour 
 {
@@ -30,6 +31,8 @@ public class ButtonManager : MonoBehaviour
     {
         main.SetActive(false);
         option.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(option.transform.GetChild(option.transform.childCount - 1).gameObject);
     }
     //Return to Main menu
     public void ReturnToMain ()
@@ -37,9 +40,22 @@ public class ButtonManager : MonoBehaviour
         resolution.SetActive(false);
         option.SetActive(false);
         main.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(main.transform.GetChild(2).gameObject);
     }
     public void ReturnToOption()
     {
+        // Checks what button to highlight based on what menu we we're in
+        if(resolution.activeSelf)
+            EventSystem.current.SetSelectedGameObject(option.transform.GetChild(0).gameObject);
+        else if (audio.activeSelf)
+            EventSystem.current.SetSelectedGameObject(option.transform.GetChild(1).gameObject);
+        else if (howToPlay.activeSelf)
+            EventSystem.current.SetSelectedGameObject(option.transform.GetChild(2).gameObject);
+        else if (main.activeSelf)
+            EventSystem.current.SetSelectedGameObject(option.transform.GetChild(3).gameObject);
+
+
         resolution.SetActive(false);
         howToPlay.SetActive(false);
         audio.SetActive(false);
@@ -49,16 +65,22 @@ public class ButtonManager : MonoBehaviour
     {
         option.SetActive(false);
         resolution.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(resolution.transform.GetChild(resolution.transform.childCount - 1).gameObject);
     }
     public void Audio()
     {
         option.SetActive(false);
         audio.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(audio.transform.GetChild(audio.transform.childCount - 1).gameObject);
     }
     public void HowToPlay()
     {
         option.SetActive(false);
         howToPlay.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(howToPlay.transform.GetChild(howToPlay.transform.childCount - 1).gameObject);
     }
 
     public void ExitBtn()
