15792e6578ef30f60d04348a35fd75e3c55bdd89
Tim Rundström <tim_rundstrom_97@hotmail.com>
2017-03-23 13:56:43 +0100

    Fixed a couple of bugs and grammar.

--- Assets/Scripts/StoryEvent_Gate.cs
+++ Assets/Scripts/StoryEvent_Gate.cs
@@ -25,9 +25,9 @@ public class StoryEvent_Gate : MonoBehaviour
         {
             "Oh, so you've finally woken up?",
             "You came through The Gate Between Worlds a couple of hours ago, but you were unconscious.",
-            "The gate is closed now but lucky for you I know how to open it again.",
+            "The gate is closed for now, but lucky for you I know how to open it again.",
             "There is a key to open it, but it's in the posession of a mighty beast in The Dark Forest.",
-            "To get to The Dark Forest you must first get through The Light Forest which is just east from here.",
+            "To get to The Dark Forest, you must first get through The Light Forest which is just east from here.",
             "This world is full of monsters so be careful!"
         };
         textBox.PrintMessage(text, "Mysterious Man", this.gameObject, "CompleteQuest");
